ccafed1f49174110aeab0d8cee4afa2e0531bca7
Raymart <71857825+rvorbita@users.noreply.github.com>
2025-08-27 17:23:48 +0800

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,10 @@ def test_health():
     resp = client.get("/health")
     assert resp.status_code == 200
     assert resp.json["status"] == "ok"
+
+
+def test_home():
+    clinet = app.app.test_client()
+    resp = clinet.get("/")
+    assert resp.status_code == 200
+    assert resp.json['message'] == "Hello, CI/CD with Jenkins in Docker!"
